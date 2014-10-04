1aa970677744ad96e431a4c27fe542fcf16abf18
naleite <naleite.nuaa@gmail.com>
2014-10-04 17:29:41 +0200

    Right verion 1729.04102014

--- src/test/java/system/TestMyPhony.java
+++ src/test/java/system/TestMyPhony.java
@@ -7,6 +7,14 @@ import java.util.Collection;
 
 import static org.junit.Assert.*;
 
+/**
+ * My own tests.
+ * Created by Leite NA on 28 Sept 2014.
+ * This is test class for PhonyList.
+ * @author NA Leite
+ * @since 28 Sept 2014
+ *
+ */
 public class TestMyPhony {
 
     /*
@@ -20,9 +28,41 @@ public class TestMyPhony {
     }
 
     /**
-     * Tests the "get" method with a list.
+     * Test the "get" method with a list with an invalid large index.
      * @see PhonyList#get(int)
      * @type Functional
+     * @input list[1,2,3] index=9
+     * @oracle The exception IndexOutOfBoundsException must rise.
+     * @passed Yes
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getElementIndexInvalidlarge(){
+        PhonyList<Integer> mylist=list(1,2,3);
+        Integer i=mylist.get(9);
+
+    }
+
+    /**
+     * Test the "get" method with a list with an invalid negative index.
+     * @see PhonyList#get(int)
+     * @type Functional
+     * @input list[1,2,3] index=-1
+     * @oracle The exception IndexOutOfBoundsException must rise.
+     * @passed Yes
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void getElementIndexInvalidNeg(){
+        PhonyList<Integer> mylist=list(1,2,3);
+        Integer i=mylist.get(-1);
+
+    }
+
+    /**
+     * Test the "get" method with a list.
+     * @see PhonyList#get(int)
+     * @type Functional
+     * @input list[1,2,3]
+     * @oracle The element got must be in the right place.
      * @passed Yes
      */
     @Test
@@ -34,9 +74,11 @@ public class TestMyPhony {
     }
 
     /**
-     * Test for "set".
+     * Test for "set" method.
      * @see system.PhonyList#set(int, Object)
      * @type Functional
+     * @input list[1] index=0 o=2
+     * @oracle The result of set must be the old value and the new value must be got in the new list.
      * @passed Yes
      */
     @Test
@@ -51,9 +93,11 @@ public class TestMyPhony {
     }
 
     /**
-     * Test for "set".
+     * Test for "set". This is a test if an index is not valid (larger than size).
      * @see system.PhonyList#set(int, Object)
      * @type Functional
+     * @input list[1] index=9
+     * @oracle The exception IndexOutOfBoundsException must rise.
      * @passed Yes
      */
     @Test(expected = IndexOutOfBoundsException.class)
@@ -67,9 +111,11 @@ public class TestMyPhony {
     }
 
     /**
-     * Test for "set".
+     * Test for "set".This is a test if an index is not valid .
      * @see system.PhonyList#set(int, Object)
      * @type Functional
+     * @input list[1] index=-1
+     * @oracle The exception IndexOutOfBoundsException must rise.
      * @passed Yes
      */
     @Test(expected = IndexOutOfBoundsException.class)
@@ -87,6 +133,7 @@ public class TestMyPhony {
          * @see PhonyList#size()
          * @type Functional
          * @input list[]
+         * @oracle The size of an empty list must be zero.
          * @passed Yes
          */
     @Test
@@ -102,6 +149,7 @@ public class TestMyPhony {
      * @see PhonyList#size()
      * @type Functional
      * @input list[]
+     * @oracle The size of a list must equal to the number of elements in the list.
      * @passed Yes
      */
     @Test
@@ -113,9 +161,10 @@ public class TestMyPhony {
     }
 
     /**
-     * Test for the "contains".
+     * Test for the method "contains".
      * @see system.PhonyList#contains(Object)
      * @type Functional
+     * @input list[1,2]
      * @oracle If an element is added to a list, then the list must contain the added element.
      * @passed Yes
      */
@@ -132,10 +181,12 @@ public class TestMyPhony {
 
     }
     /**
-     * Test for "remove".
+     * Test for "remove". This is test which for removing null from a not empty list.
      * @see system.PhonyList#remove(Object)
      * @type Functional
-     * @passed
+     * @input list[1,null] obj=null
+     * @oracle Can remove "null" normally.
+     * @passed Yes
      */
     @Test
     public void removeElementNullWithOtherElement(){
@@ -150,31 +201,14 @@ public class TestMyPhony {
         assertEquals(1,l.size());
     }
 
-    /**
-     * Test for "remove". ///Ill faut rediscuter un peu
-     * @see system.PhonyList#remove(Object)
-     * @type Functional
-     * @passed NO
-     */
-    @Test
-    public void removeElementNullWithoutOtherElement(){
-        PhonyList<Object> l=new PhonyList<>();
-        l.add(null);
-        boolean res=l.remove(null);
-        //assertFalse(res);
-        //assertEquals(1,l.size());
-        //assertEquals(0,l.indexOf(null));
-        assertTrue(res);
-        assertEquals(0,l.size());
-        assertEquals(-1,l.indexOf(null));
-
-    }
 
     /**
-     * Test for "remove".
+     * Test for "remove". Remove some elements in or not in the list.
      * @see system.PhonyList#remove(Object)
      * @type Functional
-     * @passed
+     * @input list[1,2,3] o1=1 o2=4
+     * @oracle The element contained in the list must be removed.
+     * @passed Yes
      */
     @Test
     public void removeElements(){
@@ -196,10 +230,12 @@ public class TestMyPhony {
     }
 
     /**
-     * Test for "remove".
+     * Test for "remove". To remove the only-element list.
      * @see system.PhonyList#remove(Object)
      * @type Functional
-     * @passed
+     * @input list[1] o=1
+     * @oracle The new list must be empty.
+     * @passed Yes
      */
     @Test
     public void removeTheOnlyElements(){
@@ -212,9 +248,11 @@ public class TestMyPhony {
     }
 
     /**
-     * Test for "isEmpty()"
+     * Test for "isEmpty()".
      * @see PhonyList#isEmpty()
      * @type Functional
+     * @input list[1,2] function clear()
+     * @oracle The list after clear must be empty.
      * @passed Yes
      */
     @Test
@@ -233,10 +271,12 @@ public class TestMyPhony {
     }
 
     /**
-     * Test for RemoveAll method.
+     * Test for RemoveAll method. To remove a collection of elements from an exist list.
      * @see system.PhonyList#removeAll(java.util.Collection)
      * @type Functional
-     * @passed
+     * @input list[1,2,3,4,5] collection[1,4,6,5]
+     * @oracle The new list must not contain the element which we want to remove.
+     * @passed Yes
      */
     @Test
     public void testRemoveSomeElements(){
@@ -259,55 +299,42 @@ public class TestMyPhony {
     }
 
     /**
-     * Test for RemoveAll method, if the elements to be removed are not in the list
+     * Test for RemoveAll method. To remove a collection of elements from an exist list.
      * @see system.PhonyList#removeAll(java.util.Collection)
      * @type Functional
-     * @passed
+     * @input list[1,2,3,null,4,5,1,4] collection[null,5,4,6,1]
+     * @oracle The new list must not contain the element which we want to remove.
+     * @passed Yes
      */
     @Test
-    public void testRemoveElementNotInList(){
-        PhonyList<Integer> l=list(1,2,3);
+    public void testRemoveSomeSameElements(){
+        PhonyList<Integer> l=list(1,2,3,null,4,5,1,4);
         Collection<Integer> ElementToRemove=new ArrayList<>();
-        ElementToRemove.add(4);
+        ElementToRemove.add(null);
         ElementToRemove.add(5);
+        ElementToRemove.add(4);
         ElementToRemove.add(6);
+        ElementToRemove.add(1);
         boolean res=l.removeAll(ElementToRemove);
-        assertTrue(l.contains(1));
-        assertTrue(l.contains(2));
-        assertTrue(l.contains(3));
-        assertEquals(3,l.size());
-        assertFalse(res);
-
-    }
-
-    /**
-     * Test for RemoveAll method, if the elements to be removed are not in the list
-     * @see system.PhonyList#removeAll(java.util.Collection)
-     * @type Functional
-     * @passed
-     */
-    @Test
-    public void testRemoveAllElementsInList(){
-        PhonyList<Object> l=new PhonyList<>();
-        l.add(1);
-        l.add(2);
-        l.add(3);
-        Collection<Integer> ElementToRemove=new ArrayList<>();
-        ElementToRemove.add(2);
-        ElementToRemove.add(2);
-        boolean res=l.removeAll(ElementToRemove);
-        assertTrue(l.contains(1));
-        assertFalse(l.contains(2));
-        assertTrue(l.contains(3));
+        assertFalse(l.contains(1));
+        assertFalse(l.contains(4));
+        assertFalse(l.contains(6));
+        assertFalse(l.contains(5));
+        assertEquals(2,l.size());
+        assertEquals(2,(int) l.get(0));
+        assertEquals(3,(int) l.get(1));
         assertTrue(res);
 
     }
+
 
     /**
      * Test for RemoveAll method, if there is a exception for the c.contains().
      * @see system.PhonyList#removeAll(java.util.Collection)
      * @type Functional
-     * @passed
+     * @input list[1,2,null] clt[String "e", Integer 2, Double 3.0]
+     * @oracle No exception rises and the right element can be rightly removed.
+     * @passed Yes
      */
     @Test
     public void testRemoveElementFailTry(){
@@ -329,29 +356,15 @@ public class TestMyPhony {
 
     }
 
-    /**
-     * Test for grow with a huge capacity using the method add()
-     * @see system.PhonyList#grow(int)
-     */
-    //@Test(expected = OutOfMemoryError.class)
-    public void testAddHuge(){
-        PhonyList<Integer> l = new PhonyList<>();
-        int max=Integer.MAX_VALUE-7;
-
-        for(int i=0;i<max;i++){
-            l.add(0);
-        }
-        assertEquals(0,l.indexOf(0));
-        assertEquals(0,l.indexOf(6));
-        assertEquals(max,l.size());
-    }
 
 
     /**
-     * Test for method addAll
+     * Test for method addAll. Add a collection to a list with valid index.
      * @see system.PhonyList#addAll(int, java.util.Collection)
      * @type Functional
-     * @passed
+     * @input list[0,1,2] clt[1,2]
+     * @oracle The collection can be added into the right place.
+     * @passed Yes
      */
     @Test
     public void testaddAll(){
@@ -359,23 +372,27 @@ public class TestMyPhony {
         Collection<Integer> c=new ArrayList<>();
         l.add(0);
         l.add(1);
+        l.add(2);
         c.add(1);
         c.add(2);
         boolean res=l.addAll(1,c);
-        assertEquals(4,l.size());
+        assertEquals(5,l.size());
         //assertEquals((Integer)0,l.get(0));
         assertEquals((Integer)0,l.get(0));
         assertEquals((Integer)1,l.get(1));
         assertEquals((Integer)2,l.get(2));
         assertEquals((Integer)1,l.get(3));
+        assertEquals((Integer)2,l.get(4));
         assertTrue(res);
     }
 
     /**
-     * Test for method addAll
+     * Test for method addAll. Add collection to a empty list.
      * @see system.PhonyList#addAll(int, java.util.Collection)
      * @type Functional
-     * @passed
+     * @input list[] clt[1,2]
+     * @oracle Can be normally added.
+     * @passed Yes
      */
     @Test
     public void testaddAllToEmptyList(){
@@ -397,7 +414,9 @@ public class TestMyPhony {
      * Test for method addAll with an index out of bound
      * @see system.PhonyList#addAll(int, java.util.Collection)
      * @type Functional
-     * @passed
+     * @input index=9
+     * @oracle The exception IndexOutOfBoundsException rises.
+     * @passed Yes
      */
     @Test(expected = IndexOutOfBoundsException.class)
     public void testaddAllLargeIndex(){
@@ -415,6 +434,8 @@ public class TestMyPhony {
      * Test for method addAll with a negative index out of bound
      * @see system.PhonyList#addAll(int, java.util.Collection)
      * @type Functional
+     * @input index=-1
+     * @oracle The exception IndexOutOfBoundsException rises.
      * @passed
      */
     @Test(expected = IndexOutOfBoundsException.class)
@@ -429,10 +450,12 @@ public class TestMyPhony {
     }
 
     /**
-     * Test for method addAll
+     * Test for method addAll. Add an empty collection to a list.
      * @see system.PhonyList#addAll(int, java.util.Collection)
      * @type Functional
-     * @passed
+     * @input list[1,2] clt[]
+     * @oracle The new list is same as the old one.
+     * @passed Yes
      */
     @Test
     public void testaddAllFromEmptyList(){
--- src/test/java/system/TestMyPhonyForProtected.java
+++ src/test/java/system/TestMyPhonyForProtected.java
@@ -6,9 +6,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by Leite NA on 14-10-2.
+ * My own tests for protected methods.
+ * Created by Leite NA on 28 Sept 2014.
  * This class is used for the tests with Protected
- */
+ * @author NA Leite
+ * @since 28 Sept 2014
+ * */
 public class TestMyPhonyForProtected extends PhonyList {
 
     /**
@@ -16,6 +19,8 @@ public class TestMyPhonyForProtected extends PhonyList {
      * @passed Yes
      * @type Functional
      * @see system.PhonyList#removeRange(int, int)
+     * @input list[1,2,3,4] fIdx=1, tIdx=3
+     * @oracle The new list contains the element which are not removed but does not contains the element inside of the range.
      */
     @Test
     public void testRemoveRange(){
@@ -23,16 +28,20 @@ public class TestMyPhonyForProtected extends PhonyList {
         l.add(1);
         l.add(2);
         l.add(3);
+        l.add(4);
         l.removeRange(1, 3);
-        assertEquals(1,l.size());
+        assertEquals(2,l.size());
         assertEquals((Integer)1,l.get(0));
+        assertEquals((Integer)4,l.get(1));
     }
 
     /**
-     * Some tests for removeRange(int.int). It will rise an exception.
+     * Some tests for removeRange(int.int) with a large toIndex. It will rise an exception.
      * @passed Yes
      * @type Functional   
      * @see system.PhonyList#removeRange(int, int)
+     * @input list[1,2,3] fIdx=1, tIdx=5
+     * @oracle The exception IndexOutOfBoundsException rises.
      */
     @Test(expected = IndexOutOfBoundsException.class)
     public void testRemoveRangeWithToIndexLarge(){
@@ -46,10 +55,12 @@ public class TestMyPhonyForProtected extends PhonyList {
     }
 
     /**
-     * Some tests for removeRange(int.int) It will rise an exception.
+     * Some tests for removeRange(int.int) with a large fromIndex It will rise an exception.
      * @passed Yes
      * @type Functional
      * @see system.PhonyList#removeRange(int, int)
+     * @input list[1,2,3] fIdx=5, tIdx=2
+     * @oracle The exception IndexOutOfBoundsException rises.
      */
     @Test(expected = IndexOutOfBoundsException.class)
     public void testRemoveRangeWithFromIndexLarge(){
@@ -63,10 +74,12 @@ public class TestMyPhonyForProtected extends PhonyList {
     }
 
     /**
-     * Some tests for removeRange(int.int).It will rise an exception.
+     * Some tests for removeRange(int.int) with a fromIndex=size.It will rise an exception.
      * @passed Yes
      * @type Functional
      * @see system.PhonyList#removeRange(int, int)
+     * @input list[1,2,3,4] fIdx=size, tIdx=3
+     * @oracle The exception IndexOutOfBoundsException rises.
      */
     @Test(expected = IndexOutOfBoundsException.class)
     public void testRemoveRangeWithFromIndexSize(){
@@ -76,15 +89,17 @@ public class TestMyPhonyForProtected extends PhonyList {
         l.add(3);
         l.add(4);
         int index=l.size();
-        l.removeRange(index,1);
+        l.removeRange(index,3);
 
     }
 
     /**
-     * Some tests for removeRange(int.int). It will rise an exception.
+     * Some tests for removeRange(int.int) with a negative fromIndex. It will rise an exception.
      * @passed Yes
      * @type Functional
      * @see system.PhonyList#removeRange(int, int)
+     * @input list[1,2,3] fIdx=-1, tIdx=2
+     * @oracle The exception IndexOutOfBoundsException rises.
      */
     @Test(expected = IndexOutOfBoundsException.class)
     public void testRemoveRangeWithIndexNeg(){
@@ -102,6 +117,8 @@ public class TestMyPhonyForProtected extends PhonyList {
      * @passed Yes
      * @type Functional
      * @see system.PhonyList#removeRange(int, int)
+     * @input list[1,2,3,4,5] fIdx=4, tIdx=2
+     * @oracle The exception IndexOutOfBoundsException rises.
      */
     @Test(expected = IndexOutOfBoundsException.class)
     public void testRemoveRangeWithIndexFault(){
@@ -109,7 +126,9 @@ public class TestMyPhonyForProtected extends PhonyList {
         l.add(1);
         l.add(2);
         l.add(3);
-        int index=5;
+        l.add(4);
+        l.add(5);
+        int index=4;
         l.removeRange(index, 2);
 
 
@@ -121,6 +140,8 @@ public class TestMyPhonyForProtected extends PhonyList {
      * @passed Yes
      * @type Functional
      * @see system.PhonyList#removeRange(int, int)
+     * @input list[1,2,3] fidx=2,tidx=2
+     * @oracle Nothing will happen.
      */
     @Test
     public void testRemoveRangeWithSameIndex(){
@@ -131,7 +152,6 @@ public class TestMyPhonyForProtected extends PhonyList {
         PhonyList<Integer> exp=l;
         int index=2;
         l.removeRange(index, index);
-
         assertEquals(exp,l);
     }
 }
